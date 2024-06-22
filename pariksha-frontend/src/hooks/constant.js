1ce3b60b4b20a53e99afec4ca2fdf002fe746951
SrijanTimsina <srijan.timsina.39@gmail.com>
2024-06-22 21:21:41 +0545

    update

--- pariksha-frontend/src/hooks/constant.js
+++ pariksha-frontend/src/hooks/constant.js
@@ -1,4 +1,4 @@
 export const API_URL =
-	process.env.NEXT_PUBLIC_MODE === "DEVELOPMENT"
-		? "http://localhost:3465/api/v1"
-		: "https://pariksha-27nu.onrender.com/api/v1";
+  process.env.NEXT_PUBLIC_MODE === "DEVELOPMENT"
+    ? "http://localhost:3465/api/v1"
+    : "https://pariksha-1.onrender.com/api/v1";

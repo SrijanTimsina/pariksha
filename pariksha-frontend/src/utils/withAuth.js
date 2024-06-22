ebf0874cc71b9c2cfed5a58004bfc85f7a0960f2
SrijanTimsina <srijan.timsina.39@gmail.com>
2024-06-22 20:21:42 +0545

    update

--- pariksha-frontend/src/utils/withAuth.js
+++ pariksha-frontend/src/utils/withAuth.js
@@ -4,17 +4,18 @@ import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default function withAuth(WrappedComponent) {
-  return function WithAuth(props) {
-    const session = false;
-    useEffect(() => {
-      if (!session) {
-        redirect("/login");
-      }
-    }, []);
+  // return function WithAuth(props) {
+  //   const session = false;
+  //   useEffect(() => {
+  //     if (!session) {
+  //       redirect("/login");
+  //     }
+  //   }, []);
 
-    if (!session) {
-      return null;
-    }
-    return <WrappedComponent {...props} />;
-  };
+  //   if (!session) {
+  //     return null;
+  //   }
+  //   return <WrappedComponent {...props} />;
+  // };
+  return redirect("/login");
 }

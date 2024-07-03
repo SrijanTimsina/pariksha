5b82cb4dac1e90a663c0360b2213f69db48fba5a
SrijanTimsina <srijan.timsina.39@gmail.com>
2024-07-03 23:20:53 +0545

    reset password

--- pariksha-frontend/src/hooks/constant.js
+++ pariksha-frontend/src/hooks/constant.js
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_MODE === "DEVELOPMENT"
     ? "http://localhost:3465/api/v1"
-    : "https://server.pariksha.solutions/api/v1";
+    : "https://pariksha-1.onrender.com/api/v1";
 
-// "https://pariksha-1.onrender.com/api/v1";
+// "https://server.pariksha.solutions/api/v1";
